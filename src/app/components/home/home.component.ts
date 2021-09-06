6ace43f3f71b40a63cf0333a93f17ea9427b4b0d
Dawid Roch <david.roch.2001@gmail.com>
2021-09-06 19:49:22 +0200

    sass to scss refactored

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,7 +6,7 @@ import { HttpService } from 'src/app/services/http.service';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.sass'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   public games: Game[] = [];
--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
-  styleUrls: ['./search-bar.component.sass'],
+  styleUrls: ['./search-bar.component.scss'],
 })
 export class SearchBarComponent implements OnInit {
   constructor(private router: Router) {}
