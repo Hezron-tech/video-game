698576b4aa8e1096fcb322464f5132537681b176
RochDawid <david.roch.2001@gmail.com>
2021-09-21 13:47:33 +0200

    details component created

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Game } from 'src/app/models';
+import { HttpService } from 'src/app/services/http.service';
+import { LoadingService } from 'src/app/services/loading.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  gameInfo: Game = <Game>{};
+  loading$ = this.loader.loading$;
 
-  constructor() { }
+  constructor(
+    private http: HttpService,
+    private activatedRoute: ActivatedRoute,
+    private loader: LoadingService
+  ) {}
 
   ngOnInit(): void {
+    this.activatedRoute.queryParams.subscribe((params) => {
+      this.loader.startLoading();
+      this.http.getGameInfo(params.id).subscribe((game: Game) => {
+        this.gameInfo = game;
+        console.log(game);
+        this.loader.stopLoading();
+      });
+    });
   }
-
 }
