0f731f3a96f2b1e4d927c8b936087dcc4e975889
RochDawid <david.roch.2001@gmail.com>
2021-09-21 13:50:43 +0200

    loader component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HttpHeadersInterceptor } from './interceptors/http-headers.interceptor'
 import { CardComponent } from './components/card/card.component';
 import { DetailsComponent } from './components/details/details.component';
 import { SkeletonCardComponent } from './components/skeleton-card/skeleton-card.component';
+import { LoaderComponent } from './components/loader/loader.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +24,7 @@ import { SkeletonCardComponent } from './components/skeleton-card/skeleton-card.
     CardComponent,
     DetailsComponent,
     SkeletonCardComponent,
+    LoaderComponent,
   ],
   imports: [
     BrowserModule,
